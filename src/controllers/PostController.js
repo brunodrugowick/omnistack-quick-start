8914b013574d0662b9e98cbbaf48ea34a48b7fb7
Bruno Drugowick <bruno.drugowick@gmail.com>
2019-06-13 21:45:08 -0300

    Add support to base64 images.
    Enhance model to support the image being saved.
    Changing controller to save post including new field imageBinary.

--- src/controllers/PostController.js
+++ src/controllers/PostController.js
@@ -16,23 +16,27 @@ module.exports = {
         const [name, ext] = image.split('.');
         const fileName = `${name}.jpg`;
 
+        // Generate resized image
+        const resizedImagePath = path.resolve(req.file.destination, 'resized', fileName);
         await sharp(req.file.path)
         .resize(500)
         .jpeg({ quality: 70})
-        .toFile(
-            path.resolve(req.file.destination, 'resized', fileName)
-        )
+        .toFile(resizedImagePath)
 
+        const post = new Post;
+        post.author = author;
+        post.place = place;
+        post.description = description;
+        post.hashtags = hashtags;
+        post.image = fileName,
+        post.imageBinary.data = fs.readFileSync(resizedImagePath);
+        post.imageBinary.contentType = 'image/jpeg';
+        post.save();
+
+        // Remove original image
         fs.unlinkSync(req.file.path);
 
-        const post = await Post.create({
-           author,
-           place,
-           description, 
-           hashtags, 
-           image: fileName
-        });
-
+        // Emit new post to connected clients via WebSocket.
         req.io.emit('post', post);
 
         return res.json(post);
